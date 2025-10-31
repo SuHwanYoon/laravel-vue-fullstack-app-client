24d9fe970b876afcc3ea82abaeeb63083ea630e5
SuHwanYoon <suhwan6@gmail.com>
2025-10-31 16:24:06 +0900

    상단 menu, profile사진 클릭시 sign out메뉴 초기설정

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -13,34 +13,73 @@
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="shrink-0">
-              <img class="size-8" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+              <img
+                class="size-8"
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                <!-- v-for 디렉티브는 `navigation` 배열의 각 항목에 대해 반복하여 링크를 생성합니다. -->
+                 <!-- navigation은 script블록 내부에 정의된 js객체를 넣어놓은 배열 변수입니다.-->
+                  <!-- 즉 메뉴를 반복해서 렌더링 하는것이다 -->
+                   <!-- key는 참조할 변수를 지정 -->
+                    <!-- to는 참조할 변수안에서 어디로 갈건지 지정 -->
+                <RouterLink
+                  v-for="item in navigation"
+                  :key="item.name"
+                  :to="item.to"
+                  :class="[
+                    // $route는 현재 URL과 관련된 라우트 정보가 담긴 개체로 Vue Router가 주입해주는것
+                    // 현재 페이지, URL, 라우트 이름이 뭔지 알려주는 읽기 전용 상태
+                    // 메뉴선택시 선택한 메뉴 강조 삼항 연산 로직
+                    $route.name === item.to.name
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                    'rounded-md px-3 py-2 text-sm font-medium',
+                  ]"
+                  :aria-current="$route.name === item.to.name ? 'page' : undefined">
+                  {{ item.name }}
+                </RouterLink>
               </div>
             </div>
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button type="button" class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
-                <span class="absolute -inset-1.5"></span>
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="size-6" aria-hidden="true" />
-              </button>
+
 
               <!-- Profile dropdown -->
               <Menu as="div" class="relative ml-3">
-                <MenuButton class="relative flex max-w-xs items-center rounded-full focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                <MenuButton
+                  class="relative flex max-w-xs items-center rounded-full focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
-                  <img class="size-8 rounded-full outline outline-1 -outline-offset-1 outline-white/10" :src="user.imageUrl" alt="" />
+                  <img
+                    class="size-8 rounded-full outline outline-1 -outline-offset-1 outline-white/10"
+                    :src="user.imageUrl"
+                    alt=""
+                  />
                 </MenuButton>
 
-                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform scale-100" leave-to-class="transform opacity-0 scale-95">
-                  <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-1 outline-black/5">
-                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                      <a :href="item.href" :class="[active ? 'bg-gray-100 outline-none' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                <transition
+                  enter-active-class="transition ease-out duration-100"
+                  enter-from-class="transform opacity-0 scale-95"
+                  enter-to-class="transform scale-100"
+                  leave-active-class="transition ease-in duration-75"
+                  leave-from-class="transform scale-100"
+                  leave-to-class="transform opacity-0 scale-95"
+                >
+                  <MenuItems
+                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-1 outline-black/5"
+                  >
+                  <!-- 로그아웃 버튼메뉴 -->
+                    <MenuItem>
+                      <button @click="logout"
+                        :class="['block px-4 py-2 text-sm text-gray-700',]">
+                         Sign Out
+                      </button>
                     </MenuItem>
                   </MenuItems>
                 </transition>
@@ -49,7 +88,9 @@
           </div>
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
-            <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+            <DisclosureButton
+              class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+            >
               <span class="absolute -inset-0.5"></span>
               <span class="sr-only">Open main menu</span>
               <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
@@ -58,28 +99,50 @@
           </div>
         </div>
       </div>
-
+      <!-- DisclosurePanel은  Headless UI에서 제공하는 메뉴펼치는 햄버거버튼 클릭시 나타는 패널  -->
+       <!-- md:hidden으로 인해 반응형으로 중간이상이면 숨기고 중간이하로 화면 작아지면 나타난다 -->
       <DisclosurePanel class="md:hidden">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      <!-- DisclosureBoutton은  메뉴펼치는 햄버거버튼 클릭시 나타는 패널  -->
+          <RouterLink
+            v-for="item in navigation"
+            :key="item.name"
+            :to="item.to"
+            :class="[
+              $route.name === item.to.name
+                ? 'bg-gray-900 text-white'
+                : 'text-gray-300 hover:bg-white/5 hover:text-white',
+              'block rounded-md px-3 py-2 text-base font-medium',
+            ]"
+            :aria-current="$route.name === item.to.name ? 'page' : undefined">
+              {{ item.name }}
+          </RouterLink>
         </div>
         <div class="border-t border-white/10 pb-3 pt-4">
           <div class="flex items-center px-5">
             <div class="shrink-0">
-              <img class="size-10 rounded-full outline outline-1 -outline-offset-1 outline-white/10" :src="user.imageUrl" alt="" />
+              <img
+                class="size-10 rounded-full outline outline-1 -outline-offset-1 outline-white/10"
+                :src="user.imageUrl"
+                alt=""
+              />
             </div>
             <div class="ml-3">
-              <div class="text-base/5 font-medium text-white">{{ user.name }}</div>
-              <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
+              <div class="text-base/5 font-medium text-white">
+                {{ user.name }}
+              </div>
+              <div class="text-sm font-medium text-gray-400">
+                {{ user.email }}
+              </div>
             </div>
-            <button type="button" class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
-              <span class="absolute -inset-1.5"></span>
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="size-6" aria-hidden="true" />
-            </button>
+
           </div>
           <div class="mt-3 space-y-1 px-2">
-            <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">{{ item.name }}</DisclosureButton>
+            <DisclosureButton
+              @click="logout"
+              class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">
+                Sign Out
+            </DisclosureButton>
           </div>
         </div>
       </DisclosurePanel>
@@ -87,39 +150,53 @@
 
     <header class="relative bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+          Dashboard
+        </h1>
       </div>
     </header>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- 여기에 URL path에 따른 component가 표시됨 -->
-        <RouterView/>
+        <RouterView />
       </div>
     </main>
   </div>
 </template>
 
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
 
 const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
+  name: "Tom Cook",
+  email: "tom@example.com",
   imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
 const navigation = [
-  { name: 'Upload', href: '#', current: true },
-  { name: 'My Images', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+  { name: "Upload", to: { name: "Home" } },
+  { name: "My Images", to: { name: "MyImages" } },
+];
+// 유저메뉴사용안함
+// const userNavigation = [
+//   { name: "Your profile", href: "#" },
+//   { name: "Settings", href: "#" },
+//   { name: "Sign out", href: "#" },
+// ];
+
+// 로그아웃 버튼클릭 콘솔 프린트 확인용 함수
+function logout() {
+  console.log("Logout !!!!!!");
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

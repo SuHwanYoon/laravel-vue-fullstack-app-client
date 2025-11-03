8605e9e7c557fdf5a7c030afe7616826eccaecb8
SuHwanYoon <suhwan6@gmail.com>
2025-11-03 10:56:16 +0900

    DefaultLayout,Home UI 구성완료 MyImages UI구성필요

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -148,19 +148,8 @@
       </DisclosurePanel>
     </Disclosure>
 
-    <header class="relative bg-white shadow">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-          Dashboard
-        </h1>
-      </div>
-    </header>
-    <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <!-- 여기에 URL path에 따른 component가 표시됨 -->
-        <RouterView />
-      </div>
-    </main>
+    <!-- 여기에 URL path에 따른 component가 표시됨 -->
+    <RouterView />
   </div>
 </template>
 
--- src/pages/MyImages.vue
+++ src/pages/MyImages.vue
@@ -1,13 +1,16 @@
-<script setup>
-    
-</script>
-
+<script setup></script>
 
 <template>
-  MyImages
+  <header class="relative bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">My Images</h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+
+    </div>
+  </main>
 </template>
 
-
-<style  scoped>
-
-</style>
+<style scoped></style>

a67c8bcce9233db4ce3acb0cfca8d4b3d3aa87a1
SuHwanYoon <suhwan6@gmail.com>
2025-11-05 09:56:13 +0900

    pinia, jaconfig setting

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
+//main.js에서 사용할 pinia를 선언
+const pinia = createPinia
+
+// Vue 애플리케이션을 생성하고, 라우터와 Pinia를 사용하도록 설정한 후, #app 요소에 마운트합니다.
 createApp(App)
 .use(router)
+.use(pinia)
 .mount('#app')
